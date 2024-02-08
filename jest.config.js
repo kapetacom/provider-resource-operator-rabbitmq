621305b2ed6a4ed3024724ba521cd99b176f9bc5
Henrik Hofmeister <henrik@newdawn.dk>
2024-02-08 10:15:53 +0100

    fix: Tests

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
     preset: 'ts-jest',
+    moduleNameMapper: {
+        '\.ya?ml$': '<rootDir>/__mocks__/yaml.js',
+    }
 };
